f5f514b36363b5ddfb49e146d7bb737c9804c052
leo <lihangleo2@163.com>
2019-08-21 14:17:27 +0800

    Update SystemConst.java

--- app/src/main/java/com/lihang/selfmvp/common/SystemConst.java
+++ app/src/main/java/com/lihang/selfmvp/common/SystemConst.java
@@ -7,6 +7,6 @@ package com.lihang.selfmvp.common;
 public class SystemConst {
     //域名
     public static final String DEFAULT_SERVER = "http://gank.io/api/";
-    public static final String DIFFERT_URL = "https://ijixin.com/brain/rest/assessmentRecord/upload";
+    public static final String DIFFERT_URL = "http://server.jeasonlzy.com/OkHttpUtils/upload";
     public static final String QQ_APK = "https://imtt.dd.qq.com/16891/apk/06AB1F5B0A51BEFD859B2B0D6B9ED9D9.apk?fsname=com.tencent.mobileqq_8.1.0_1232.apk&csr=1bbd";
 }
